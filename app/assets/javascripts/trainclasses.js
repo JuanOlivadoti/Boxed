197c2d2bfd3005967f3d226dcde1d73d108e5b85
Juan Pablo OLIVADOTI <jp.olivadoti@gmail.com>
2016-12-07 18:58:07 +0100

    Continue with association

--- app/assets/javascripts/trainclasses.js
+++ app/assets/javascripts/trainclasses.js
@@ -9,12 +9,17 @@ function handleOnError (error) {
 }
 
 $(document).ready(function(){
+	event.preventDefault;
 	console.log("document ready");
+
 	$('#js-book').click(function(event){
 		console.log("loaded");
 
 		var classId = $('[data-train-id]').data('train-id');
 		var userId	= $('[data-user-id]').data('user-id');
+
+		console.log(classId);
+		console.log(userId);
 
 		$.ajax({
 			type: "POST",
